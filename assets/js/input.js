55ac03d7a1c355468b3ce5fffc9fa39a65f8b3d6
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-07-03 16:42:15 +0530

    Input field js

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,1 +1,9 @@
+function upload(){
+    var uploader = document.getElementById("uploader");
+    uploader.click();
+}
+
 var uploader = document.getElementById("uploader");
+if( document.getElementById("uploader").files.length == 0 ){
+    console.log("no files selected");
+}
