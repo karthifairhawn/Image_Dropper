06759a8c8e5d78f07dc1dbae2319f2af64b1af33
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-07-04 18:32:09 +0530

    Hide plus button on image uplaod

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,9 +1,20 @@
 function upload(){
     var uploader = document.getElementById("uploader");
     uploader.click();
+    uploaded_check()
 }
 
-var uploader = document.getElementById("uploader");
-if( document.getElementById("uploader").files.length == 0 ){
-    console.log("no files selected");
+function uploaded_check(){
+    upload_check_int = setInterval( function(){
+        var uploader = document.getElementById("uploader");
+        if( document.getElementById("uploader").files.length == 0 ){
+            console.log("no files selected");
+        }else{
+            console.log("selected");
+            clearInterval(upload_check_int);
+            document.getElementById("upload_plus").classList.add("uploaded");
+        }
+    
+    }, 2000 );
+    
 }
